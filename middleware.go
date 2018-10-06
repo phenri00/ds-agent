7bec6c822d0fb85024c39008c4e0b2eb92b8940c
Patrick Henriksson <henriksson@Patricks-MacBook-Air.local>
2018-10-06 19:17:33 +0200

    renamed:    ds_agent.go -> main.go 	modified:   middleware.go

--- middleware.go
+++ middleware.go
@@ -7,8 +7,11 @@ import (
 
 func middleWareAuth(secret string, h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+
+		log.Print(r.RemoteAddr, " ", r.Method, " ", r.URL)
+
 		if r.Header.Get("x-auth") != secret {
-			log.Print("ERROR: Unauthorized. Remote addr: ", r.RemoteAddr)
+			log.Print("ERROR: Unauthorized")
 			http.Error(w, "Not Authorized", http.StatusUnauthorized)
 			return
 		}
